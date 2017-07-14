f840322370e32f39cdd0f54c80500d4a2380a6f0
Pragun Sharma <psharma5@ucsc.edu>
2017-07-13 18:19:28 -0700

    UI Set Up Complete

---
+++ MyWeatherApp/WeatherViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by Pragun Sharma on 13/07/17.
+//  Copyright © 2017 Pragun Sharma. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherData", for: indexPath)
+        return cell
+    }
+}
+
